5f354371c25e416245bc703e80c5c1d2caff88d4
MAI QUOC VIET <143470149+tfboys2016@users.noreply.github.com>
2024-08-25 22:44:55 +0700

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path, include
-from .views import *
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
+from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    path('api/login/', LoginView.as_view(), name='login'),
-    path('api/user/', UserDetailView.as_view(), name='user-detail'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
-    path('', include(router.urls)),
+    path('api/register/user',views.RegisterView.as_view(), name='api-register-user'),
+    path('api/login/',views.LoginView.as_view(), name='api-login-user'),
+    path('api/user/', views.UserDetailView.as_view(), name='api-detail-user'),
+    path('api/token/refresh/', views.TokenRefreshView.as_view(), name='refresh-token'),
+    path('api/update/user/',views.UpdateUserView.as_view(), name='api-update-user')
 ]
